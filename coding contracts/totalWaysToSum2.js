c806314c127ee1f28ac75cba1951bfca7c84f3df
Nick Sullivan <nelsontorin1@gmail.com>
2023-12-28 20:47:21 -0800

    Remove redundant code and fix function parameter issue

--- coding contracts/totalWaysToSum2.js
+++ coding contracts/totalWaysToSum2.js
@@ -12,8 +12,6 @@ function recurse(lst, num, index, total) {
 }
 
 function main(lst, num) {
-    const lst = [1, 2, 9, 10, 11, 14, 16, 17, 18, 21, 23];
-    const num = 125;
     var index = 0;
     var total = 0;
     return recurse(lst, num, index, total);
