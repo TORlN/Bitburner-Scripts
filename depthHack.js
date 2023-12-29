24739dc031e3b89baa9ac8e2dedefdad985e2665
Nick Sullivan <nelsontorin1@gmail.com>
2023-12-29 03:01:45 -0800

    improved contract solving logic and add logging

--- depthHack.js
+++ depthHack.js
@@ -12,18 +12,18 @@ async function hack(ns, server, depth, verbose, hackVerbose) {
         while (ns.isRunning(pid)) {
             await ns.sleep(1)
         }
-        // var f = ns.ls("home", "contract")
-        // f.shift();
-        // f.shift();
-        // ns.tprint(f);
+        var f = ns.ls("home", "contract")
         var files = ns.ls(server, "contract");
         if (files.length > 0) {
             for (let i = 0; i < files.length; i++) {
                 await ns.exec("solveContracts.js", "home", { preventDuplicates: true }, server, files[i], hackVerbose);
             }
-        }// else if (f.length > 0) {
-        // await ns.exec("solveContracts.js", "home", { preventDuplicates: true }, "home", f[0]);
-        // }
+        } else if (f.length > 0) {
+            await ns.exec("solveContracts.js", "home", { preventDuplicates: true }, "home", f[0], hackVerbose);
+        } else {
+            ns.tprint("creating dummy contract");
+            ns.codingcontract.createDummyContract("Total Ways to Sum II");
+        }
     }
     for (let i = 0; i < neighbors.length; i++) {
         if (serverSet.has(neighbors[i])) {
--- solveContracts.js
+++ solveContracts.js
@@ -1,23 +1,43 @@
 function findDistinctCombos(num, maximum, memo = new Map()) {
-    // Check if the result for these parameters is already in the memo
     const key = `${num}-${maximum}`;
     if (memo.has(key)) {
         return memo.get(key);
     }
-
-    // Base cases
     if (num === 0) {
         return 1;
     } else if (num < 0 || maximum === 0) {
         return 0;
     }
-
-    // Recursive calls with memoization
     const result = findDistinctCombos(num - maximum, maximum, memo) + findDistinctCombos(num, maximum - 1, memo);
-
-    // Store the result in the memo before returning
     memo.set(key, result);
     return result;
+}
+
+function arrayCombos(num, lst, index, total, memo = new Map()) {
+    const key = `${num}-${index}-${total}`;
+    if (memo.has(key)) {
+        return memo.get(key);
+    }
+    if (total === num) {
+        return 1;
+    }
+    if (total > num) {
+        return 0;
+    }
+    if (index >= lst.length) {
+        return 0;
+    }
+    const result = arrayCombos(num, lst, index, total + lst[index], memo) + arrayCombos(num, lst, index + 1, total, memo);
+    memo.set(key, result);
+    return result;
+}
+
+function printLogging(ns, str, type, server) {
+    if (str != "") {
+        ns.tprint("\u001b[35m" + "Solved " + type + " contract on " + server + "\u001b[0m");
+    } else {
+        ns.tprint("\u001b[31m" + "Failed to solve " + type + " contract on " + server + "\u001b[0m");
+    }
 }
 /** @param {NS} ns */
 export async function main(ns) {
@@ -28,20 +48,18 @@ export async function main(ns) {
         case "Total Ways to Sum":
             var data = await ns.codingcontract.getData(file, server);
             var soln = await findDistinctCombos(data, data) - 1;
-            if (await ns.codingcontract.attempt(soln, file, server) != "" && verbose) {
-                ns.tprint("Solved " + file + " on " + server);
-            } else if (verbose) {
-                ns.tprint("Failed to solve " + file + " on " + server);
+            var str = await ns.codingcontract.attempt(soln, file, server);
+            if (verbose) {
+                printLogging(ns, str, "Total Ways to Sum", server);
             }
             break;
         case "Total Ways to Sum II":
             var data = await ns.codingcontract.getData(file, server);
-            // var soln = await ns.run("./coding%20contracts/totalWaysToSum2.js", 1, data[0], data[1]);
-            // if (ns.codingcontract.attempt(soln, files[i], server) != "") {
-            //     ns.tprint("SUCCESS Solved " + files[i] + " on " + server);
-            // } else {
-            //     ns.tprint("ERROR Failed to solve " + files[i] + " on " + server);
-            // }
+            var soln = await arrayCombos(data[0], data[1], 0, 0);
+            var str = await ns.codingcontract.attempt(soln, file, server);
+            if (verbose) {
+                printLogging(ns, str, "Total Ways to Sum II", server);
+            }
             break;
         default:
 
